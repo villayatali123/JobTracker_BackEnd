f4087e5f762f01cd4476428596ee0b368e1d3ea3
Villayat Ali <villayat@niflr.com>
2023-09-22 15:24:48 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 
 ///extra security packages
 
-const helmet = require("helmet");
-// const cors = require("cors");
+// const helmet = require("helmet");
+const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
+// const rateLimiter = require("express-rate-limit");
 
 const authenticateUser = require("./middleware/authentication");
 
@@ -28,16 +28,16 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.set("trust proxy", 1);
 
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, //15Minutes
-    max: 100,
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, //15Minutes
+//     max: 100,
+//   })
+// );
 
 app.use(express.json());
-app.use(helmet());
-// app.use(cors());
+// app.use(helmet());
+app.use(cors());
 app.use(xss());
 // enable CORS requests
 const allowCrossDomain = (req, res, next) => {
