8c98e0e1eecf9fdbd7b54cad131f3c4f44e9e452
Villayat Ali <villayat@niflr.com>
2023-09-22 14:04:05 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 ///extra security packages
 
 const helmet = require("helmet");
-const cors = require("cors");
+// const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
@@ -37,13 +37,20 @@ app.use(
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(xss());
+// enable CORS requests
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+  next();
+}
+app.use(allowCrossDomain);
 
 // routes
 
 app.get("/", (req, res) => {
-  res.send("Starting the Project from here");
+  res.send("Welcome to beginning of nothingness");
 });
 
 app.use("/api/v1/auth", authRouter);
