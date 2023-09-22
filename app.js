b655e6080ca1f3eba4160e9caad29b5cbdf1ca6f
Villayat Ali <villayat@niflr.com>
2023-09-22 15:06:03 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(xss());
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 }
 app.use(allowCrossDomain);
